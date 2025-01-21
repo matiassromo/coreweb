70619f1dfd2782997edae5e72b2a49e2ef56bff2
matiassromo <matiasromo10@hotmail.com>
2025-01-21 00:30:07 -0500

    app.py fix

--- app.py
+++ app.py
@@ -38,4 +38,6 @@ if __name__ == "__main__":
     # Crear las tablas si no existen
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    # Ejecutar la aplicación en el puerto proporcionado por Azure
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
